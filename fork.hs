e585c81d0b8d577c6dd537b8ee69902ad14f7a92
Simon Marlow <marlowsd@gmail.com>
2012-10-23 13:53:46 +0100

    port splicecode to asciidoc, and re-splice everything

--- fork.hs
+++ fork.hs
@@ -1,9 +1,11 @@
+-- <<fork
 import Control.Concurrent
 import Control.Monad
 import System.IO
 
 main = do
-  hSetBuffering stdout NoBuffering
-  forkIO $ forever $ putChar 'A'
-  forkIO $ forever $ putChar 'B'
-  threadDelay (10^6)
+  hSetBuffering stdout NoBuffering -- <1>
+  forkIO $ forever $ putChar 'A'   -- <2>
+  forkIO $ forever $ putChar 'B'   -- <2>
+  threadDelay (10^6)               -- <3>
+-- >>
