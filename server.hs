bd27150eac441a57ed58b6506cc90300be4300ab
Simon Marlow <marlowsd@gmail.com>
2012-11-16 16:49:22 +0000

    start on conc-server

--- server.hs
+++ server.hs
@@ -1,6 +1,7 @@
+import ConcurrentUtils
 import Network
 import Control.Monad
-import Control.Concurrent
+import Control.Concurrent (forkIO)
 import System.IO
 import Text.Printf
 import Control.Exception
@@ -8,22 +9,27 @@ import Control.Exception
 port :: Int
 port = 44444
 
+-- <<main
 main = withSocketsDo $ do
-  sock <- listenOn (PortNumber (fromIntegral port))
+  sock <- listenOn (PortNumber (fromIntegral port))              -- <1>
   printf "Listening on port %d\n" port
-  forever $ do
-     (handle, host, port) <- accept sock
+  forever $ do                                                   -- <2>
+     (handle, host, port) <- accept sock                         -- <3>
      printf "Accepted connection from %s: %s\n" host (show port)
-     forkIO (talk handle `finally` hClose handle)
+     forkFinally (talk handle) (\_ -> hClose handle)             -- <4>
+-- >>
 
+-- <<talk
 talk :: Handle -> IO ()
 talk h = do
-  hSetBuffering h LineBuffering
-  loop
+  hSetBuffering h LineBuffering                                -- <1>
+  loop                                                         -- <2>
  where
   loop = do
-    line <- hGetLine h
-    if line == "end"
-       then hPutStrLn h ("Thank you for using the " ++
+    line <- hGetLine h                                         -- <3>
+    if line == "end"                                           -- <4>
+       then hPutStrLn h ("Thank you for using the " ++         -- <5>
                          "Haskell doubling service.")
-       else do hPutStrLn h (show (2 * (read line :: Integer))); loop
+       else do hPutStrLn h (show (2 * (read line :: Integer))) -- <6>
+               loop                                            -- <7>
+-- >>
