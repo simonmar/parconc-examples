8b16e630461d2279ca8da6451491e892e3e8fb7d
Simon Marlow <marlowsd@gmail.com>
2012-11-27 17:07:06 +0000

    conc-distrib

--- distrib-ping/ping.hs
+++ distrib-ping/ping.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE TemplateHaskell, DeriveDataTypeable #-}
-import Remote
-import Control.Monad.IO.Class
-import Control.Monad
+{-# OPTIONS_GHC -Wall #-}
+import Control.Distributed.Process
+import Control.Distributed.Process.Closure
+import Control.Distributed.Process.Node (initRemoteTable)
+import Control.Distributed.Static hiding (initRemoteTable)
+
+import DistribUtils
+
 import Text.Printf
-import Control.Concurrent
 import Data.DeriveTH
 import Data.Binary
 import Data.Typeable
@@ -13,39 +17,41 @@ data Message = Ping ProcessId
              | Pong ProcessId
   deriving Typeable
 
-$( derive makeBinary ''Message )
+derive makeBinary ''Message
 -- >>
 
 -- <<pingServer
-pingServer :: ProcessM ()
+pingServer :: Process ()
 pingServer = do
   Ping from <- expect
+  say $ printf "ping received from %s" (show from)
   mypid <- getSelfPid
   send from (Pong mypid)
 -- >>
 
 -- <<remotable
-$( remotable ['pingServer] )
+remotable ['pingServer]
 -- >>
 
 -- <<master
-master :: ProcessM ()
+master :: Process ()
 master = do
   node <- getSelfNode
 
   say $ printf "spawning on %s" (show node)
-  pid <- spawn node pingServer__closure
+  pid <- spawn node (staticClosure pingServer__static)
 
   mypid <- getSelfPid
-  say $ printf "pinging %s" (show pid)
+  say $ printf "sending ping to %s" (show pid)
   send pid (Ping mypid)
 
   Pong _ <- expect
   say "pong."
+
   terminate
 -- >>
 
 -- <<main
-main = remoteInit (Just "config") [Main.__remoteCallMetaData] $
-         \_ -> master
+main :: IO ()
+main = distribMain master (Main.__remoteTable initRemoteTable)
 -- >>
