e3a31369da383865c9964a181b773be8fee6c250
Simon Marlow <marlowsd@gmail.com>
2013-02-22 16:37:03 +0000

    checkpoint

--- timetable.hs
+++ timetable.hs
@@ -6,98 +6,116 @@ import System.Random
 import System.Environment
 import Debug.Trace
 import Data.List
+import Control.Monad.Par
 
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Data.Set (Set)
-import qualified Data.Set as Set
 
-newtype Class = Class Int
+newtype Talk = Talk Int
   deriving (Eq,Ord)
 
-instance Show Class where
-  show (Class t) = show t
+instance NFData Talk
 
-data Student = Student
+instance Show Talk where
+  show (Talk t) = show t
+
+data Person = Person
   { name    :: Text
-  , classes :: [Class]
+  , talks :: [Talk]
   }
   deriving (Show)
 
-type TimeTable = [[Class]]
+type TimeTable = [[Talk]]
 
 selects xs [] = []
 selects xs (y:ys) = (y,xs++ys) : selects (y:xs) ys
 
-solve :: [Student] -> [Class] -> Int -> Int -> [TimeTable]
-solve students all_classes maxDay maxSlot =
+solve :: [Person] -> [Talk] -> Int -> Int -> Par [TimeTable]
+solve persons all_talks maxTrack maxSlot =
   trace (show clashesWith) $
-  generate all_classes all_classes 0 [] 0 []
+  generate 0 all_talks all_talks 0 [] 0 []
  where
-  clashesWith :: Map Class (Set Class)
-  clashesWith = Map.fromListWith Set.union
-     [ (c, Set.fromList cs)
-     | s <- students, (c, cs) <- selects [] (classes s) ]
+  clashesWith :: Map Talk [Talk]
+  clashesWith = Map.fromListWith (\xs ys -> filter (`notElem` ys) xs ++ ys)
+     [ (c, cs)
+     | s <- persons, (c, cs) <- selects [] (talks s) ]
 
-  generate this_slot cs slot slots day days
---    | trace (show slot ++ " " ++ show day ++ " " ++ show this_slot) False = undefined
+  generate depth this_slot cs slot slots track tracks
+--    | trace (show slot ++ " " ++ show track ++ " " ++ show this_slot) False = undefined
+    | slot == maxSlot = return [ slots ]
+    | track == maxTrack   = generate depth cs cs (slot+1) (tracks:slots) 0 []
+    | otherwise = do
+      if (depth > 3)
+         then return (generate_seq this_slot cs slot slots track tracks)
+         else do
+           is <- mapM spawn next
+           xss <- mapM get is
+           return (concat xss)
+    where
+     next =  [ generate (depth+1) this_slot' (filter (/= c) cs) slot slots
+                        (track+1) (c:tracks)
+             | (c,ts') <- selects [] this_slot
+               let clashes = Map.findWithDefault [] c clashesWith
+               let this_slot' = filter (`notElem` clashes) ts'
+             ]
+
+  generate_seq this_slot cs slot slots track tracks
     | slot == maxSlot = [ slots ]
-    | day == maxDay   = generate cs cs (slot+1) (days:slots) 0 []
+    | track == maxTrack   = generate_seq cs cs (slot+1) (tracks:slots) 0 []
     | otherwise =
-      concat [ generate (filter (`notElem` Set.toList (Map.findWithDefault Set.empty c clashesWith)) ts') (filter (/= c) cs) slot slots (day+1) (c:days)
+       concat [ generate_seq (filter (`notElem` (Map.findWithDefault [] c clashesWith)) ts') (filter (/= c) cs) slot slots (track+1) (c:tracks)
              | (c,ts') <- selects [] this_slot
              ]
 
-   -- Keep a list of classes we can put in this slot
+   -- Keep a list of talks we can put in this slot
    -- if it is empty, backtrack
    -- For each class,
-   -- Pick one of their classes that is in our list of classes
+   -- Pick one of their talks that is in our list of talks
    --
 
 
 -- ----------------------------------------------------------------------------
 
-bench :: Int -> Int -> Int -> Int -> Int -> StdGen -> ([Student],[Class],[TimeTable])
-bench nslots ndays nclasses nstudents c_per_s gen =
-  trace (show classes) $
-  (students,classes,solve students classes ndays nslots)
+bench :: Int -> Int -> Int -> Int -> Int -> StdGen -> ([Person],[Talk],[TimeTable])
+bench nslots ntracks ntalks npersons c_per_s gen =
+  (persons,talks, runPar $ solve persons talks ntracks nslots)
  where
-  total_classes = nslots * ndays
+  total_talks = nslots * ntracks
 
-  classes = map Class [1..total_classes]
-  students = mkstudents nstudents gen
+  talks = map Talk [1..total_talks]
+  persons = mkpersons npersons gen
 
-  mkstudents :: Int -> StdGen -> [Student]
-  mkstudents 0 g = []
-  mkstudents n g = Student (Text.pack ('S':show n)) (take c_per_s cs) : rest
+  mkpersons :: Int -> StdGen -> [Person]
+  mkpersons 0 g = []
+  mkpersons n g = Person (Text.pack ('S':show n)) (take c_per_s cs) : rest
         where
           (g1,g2) = split g
-          rest = mkstudents (n-1) g2
-          cs = nub [ classes !! n | n <- randomRs (0,nclasses-1) g ]
+          rest = mkpersons (n-1) g2
+          cs = nub [ talks !! n | n <- randomRs (0,ntalks-1) g ]
 
 main = do
    [ a, b, c, d, e ] <- fmap (fmap read) getArgs
    let g = mkStdGen 1001
    let (ss,cs,ts) = bench a b c d e g
    print ss
-   print (head ts)
+   print (length ts)
 --   [ a, b ] <- fmap (fmap read) getArgs
 --   print (head (test2 a b))
 
-test = solve testStudents cs 2 2
+test = solve testPersons cs 2 2
  where
-   cs@[ca,cb,cc,cd] = map Class [1..4]
+   cs@[ca,cb,cc,cd] = map Talk [1..4]
 
-   testStudents =
-    [ Student "P" [ca,cb]
-    , Student "Q" [cb,cc]
-    , Student "R" [cc,cd]
+   testPersons =
+    [ Person "P" [ca,cb]
+    , Person "Q" [cb,cc]
+    , Person "R" [cc,cd]
     ]
 
-test2 n m = solve testStudents cs m n
+test2 n m = solve testPersons cs m n
  where
-   cs = map Class [1 .. (n * m)]
+   cs = map Talk [1 .. (n * m)]
 
-   testStudents =
-    [ Student "1"  (take n cs)
+   testPersons =
+    [ Person "1"  (take n cs)
     ]
