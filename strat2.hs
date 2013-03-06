c263dca2405d25bdb16dcfd9c325da21162fbc2d
Simon Marlow <marlowsd@gmail.com>
2013-03-06 14:36:23 +0000

    Two Strategies examples

---
+++ strat2.hs
@@ -0,0 +1,33 @@
+import Control.Parallel
+import Control.Parallel.Strategies (rpar, Strategy, using)
+import Control.Exception
+import Data.Time.Clock
+import Text.Printf
+import System.Environment
+
+-- <<fib
+fib :: Integer -> Integer
+fib 0 = 1
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+-- >>
+
+main = print pair
+ where
+  pair =
+-- <<pair
+   (fib 35, fib 36) `using` evalPair rpar rpar
+-- >>
+
+-- <<evalPair
+evalPair :: Strategy a -> Strategy b -> Strategy (a,b)
+evalPair sa sb (a,b) = do
+  a' <- sa a
+  b' <- sb b
+  return (a',b')
+-- >>
+
+-- <<parPair
+parPair :: Strategy (a,b)
+parPair = evalPair rpar rpar
+-- >>
