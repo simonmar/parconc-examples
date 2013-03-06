e434ec0820af7bd5ad6d034155f9c6a0b963fc9d
Simon Marlow <marlowsd@gmail.com>
2013-03-06 14:38:04 +0000

    the strategies version

--- sudoku5.hs
+++ sudoku5.hs
@@ -1,13 +1,17 @@
 import Sudoku
 import Control.Exception
 import System.Environment
-import Control.Parallel.Strategies hiding (parMap)
-import Control.DeepSeq
+import Control.Parallel.Strategies
 import Data.Maybe
 
 main :: IO ()
 main = do
-    [f] <- getArgs
-    grids <- fmap lines $ readFile f
-    evaluate (length grids)
-    print $ length $ filter isJust $ (map solve grids `using` parList rseq)
+  [f] <- getArgs
+  file <- readFile f
+
+  let puzzles   = lines file
+-- <<solutions
+  let solutions = map solve puzzles `using` parList rseq
+-- >>
+
+  print (length (filter isJust solutions))
