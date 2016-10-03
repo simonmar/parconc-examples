f1bc24579b496cf820dab8c26d74cfe36ce11ac8
Simon Marlow <marlowsd@gmail.com>
2016-10-03 12:53:58 +0100

    Fix build with GHC < 7.10

--- GetURL.hs
+++ GetURL.hs
@@ -7,6 +7,7 @@ module GetURL (getURL) where
 import Network.HTTP.Conduit
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as L
+import Control.Applicative -- for GHC < 7.10
 
 getURL :: String -> IO ByteString
 getURL url = L.toStrict <$> simpleHttp url
