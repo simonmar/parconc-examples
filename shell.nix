5e2e44444fe9605dbbf8ce0fd6c52e1b614cb195
Tobi Oloke <tobzdaman619@gmail.com>
2024-10-25 13:06:57 +0100

    Rank 1 Dynamic fails test for some reason

--- shell.nix
+++ shell.nix
@@ -1,11 +1,17 @@
 let 
   nixpkgs = import (builtins.fetchTarball {
-            url = "https://github.com/NixOS/nixpkgs/archive/5c79b3dda06744a55869cae2cba6873fbbd64394.tar.gz";}) {}; 
+            url = "https://github.com/NixOS/nixpkgs/archive/5c79b3dda06744a55869cae2cba6873fbbd64394.tar.gz";}) {};
+  
+  compiler = "ghc884";
 
-  hpkgs = nixpkgs.haskell.packages."ghc884";
+  hpkgs = nixpkgs.haskell.packages.${compiler};
   # hs = import ./parconc-examples.nix;
+
+  parconc-pkgs = import ./parconc-examples.nix {inherit nixpkgs compiler; doBenchmark = false; };
+
 in 
   hpkgs.shellFor {
+    inherit parconc-pkgs;
     name = "parconc-examples";
     packages = _: [];
     nativeBuildInputs = with hpkgs; [ 
