ba8659497924f178bb29b2ae13698aef923355c8
Tobi Oloke <tobzdaman619@gmail.com>
2024-10-27 08:27:47 +0000

    As good as it gets

--- shell.nix
+++ shell.nix
@@ -1,36 +1,37 @@
 let 
   nixpkgs = import (builtins.fetchTarball {
-            url = "https://github.com/NixOS/nixpkgs/archive/a5c9c6373aa35597cd5a17bc5c013ed0ca462cf0.tar.gz";}) {};
+            url = "https://github.com/NixOS/nixpkgs/archive/59b8d9cf24e9fcf10341a0923c9bdca088dca8c8.tar.gz";}) {};
  
-  modern-nixpkgs = import <nixpkgs> {};
-  compiler = "ghc822";
+  modern-nixpkgs = import <unstable> {};
+  compiler = "ghc884";
 
   hpkgs = nixpkgs.haskell.packages.${compiler};
-  # hs = import ./parconc-examples.nix;
 
   # ghc-events-pkgs = import (builtins.fetchTarball {
   #                     url = "https://github.com/NixOS/nixpkgs/archive/7cc52df39e93d427960646a1797362048fa46011.tar.gz";
   #                   }) {};
 
-  parconc-pkgs = import ./parconc-examples.nix {inherit nixpkgs compiler; doBenchmark = false; };
+  parconc-pkgs = import ./parconc-examples.nix {inherit nixpkgs compiler; };
 
 in 
-  hpkgs.shellFor {
+  hpkgs.shellFor rec {
     # inherit parconc-pkgs;
     name = "parconc-examples";
     packages = _: [];
     nativeBuildInputs = with hpkgs; [ 
       modern-nixpkgs.stack
       modern-nixpkgs.cabal-install
-      # (nixpkgs.haskell-language-server.override { supportedGhcVersions = [ compiler ];})
+      haskell-language-server
       # ghc-events
       # hvega 
-      # eventlog2html
+      modern-nixpkgs.haskellPackages.eventlog2html
       # (final: prev: {final.hpkgs.eventlog2html =  prev.hpkgs.eventlog2html.override { hvega = prev.hpkgs.hvega; }; })
-      nixpkgs.pkgconfig # Required to build some packages
+      nixpkgs.pkg-config # Required to build some packages
       nixpkgs.zlib # Required to build some packages
       parallel
       modern-nixpkgs.haskellPackages.implicit-hie
-      modern-nixpkgs.haskellPackages.threadscope
-  ]; # ++ hs.hackages hpkgs;
+      threadscope
+    ]; 
+
+    LD_LIBRARY_PATH = modern-nixpkgs.lib.makeLibraryPath (with nixpkgs; [ pkg-config zlib ]); 
 }
